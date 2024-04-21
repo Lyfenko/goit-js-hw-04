5e95c1242acd28748d7051444668de40d1c0a717
Dmytro Lyfenko <114363730+Lyfenko@users.noreply.github.com>
2024-04-21 16:41:46 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,24 @@
+function isEnoughCapacity(product, containerSize) {
+    let result = true;
+    for (const key in product) {
+        if (product[key] > containerSize) {
+            result = false;
+        }
+    }
+    return result;
+}
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
